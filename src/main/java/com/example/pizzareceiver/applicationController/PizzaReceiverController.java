528542fed813d6bc375bdaac126e8b89cd172a53
Z6WOD <wod@kmd.dk>
2023-08-03 20:38:00 +0200

    dodanie kubernetesa

--- src/main/java/com/example/pizzareceiver/applicationController/PizzaReceiverController.java
+++ src/main/java/com/example/pizzareceiver/applicationController/PizzaReceiverController.java
@@ -1,5 +1,6 @@
 package com.example.pizzareceiver.applicationController;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,10 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class PizzaReceiverController {
+
+    private static final Logger logger = LoggerFactory.getLogger(PizzaReceiverController.class);
+
     @PostMapping("/pizza-receiver")
     public ResponseEntity<String> receivePizza(@RequestBody Pizza pizza) {
-        System.out.println("Received pizza:");
-        System.out.println(pizza.getName() + " - " + pizza.getIngredients() + " - " + pizza.getPrice() + " PLN");
+        // Use the logger to log the received pizza details
+        logger.info("Received pizza:");
+        logger.info(pizza.getName() + " - " + pizza.getIngredients() + " - " + pizza.getPrice() + " PLN");
         return ResponseEntity.ok("Pizza received successfully!");
     }
 }
